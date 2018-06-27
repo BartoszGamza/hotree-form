5d7b8351f5419d7fd286babb4c2f66617a68cbfc
bartoszgamza <bartosz.gamza12@gmail.com>
2018-06-27 23:00:09 +0200

    implemented cards

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,13 +1,35 @@
 <template>
   <div class="container">
     <div class="card">
-      <h1>this is a card1</h1>
+      <div class="card-heading">
+        About
+        <hr>
+      </div>
+      <div class="card-form">
+        <label for="title">TITLE</label>
+        <input type="text" id="title" placeholder="Make it short and clear">
+        <label for="desc">DESCRIPTION</label>
+        <input type="text" id="desc" placeholder="Write about your event, be creative">
+        
+      </div>
     </div>
     <div class="card">
-      <h1>this is a card2</h1>
+      <div class="card-heading">
+        Coordinator
+        <hr>
+      </div>
+      <div class="card-form">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus provident sequi neque itaque eos perferendis doloremque sunt cupiditate amet, eum culpa quas delectus reprehenderit suscipit rem minima assumenda odio nemo!
+      </div>
     </div>
     <div class="card">
-      <h1>this is a card2</h1>
+      <div class="card-heading">
+        When
+        <hr>
+      </div>
+      <div class="card-form">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus provident sequi neque itaque eos perferendis doloremque sunt cupiditate amet, eum culpa quas delectus reprehenderit suscipit rem minima assumenda odio nemo!
+      </div>
     </div>
   </div>
 </template>
@@ -30,6 +52,14 @@ export default {
   margin-bottom 25px
   max-width 800px
   box-shadow 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)
-  padding-top 3px
   background-color white
+  border-radius 5px
+.card-heading
+  color #436598
+  padding 15px
+
+.card-form
+  padding 15px
+
+
 </style>
